314210e3dc8d8c792f253bcf09c494d46f80853c
th-shivam <dreamyshivam01@gmail.com>
2026-08-28 19:53:37 +0530

    Fixed CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Reacher API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://reacher-tawny.vercel.app/"],
+    allow_origins=["https://reacher-tawny.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
